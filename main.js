b97f3fb884ff3611398fa68e8bf26b43476963db
Josue Mendez <josuejoelmm@gmail.com>
2019-07-31 22:02:25 -0300

    actualizando cambios del ejercicio

--- main.js
+++ main.js
@@ -4,3 +4,36 @@
  *  - rendering: Es lo que tarda el interprete 
  * 
  */
+var msj = `
+    <div id="container">
+        <p>Esta seguro que quiere abandonar la pagina?</p>
+        <div>
+            <a href="#" id="aceptar">Aceptar</a>
+            <a href="#" id="cancelar">Cancelar</a>
+        </div>
+    </div>
+    `;
+var link = document.getElementsByTagName('a')[0];
+link.addEventListener('click', e => {
+    e.preventDefault();
+    console.log('asdf');
+    var msj = `
+    <div id="container">
+        <p>Esta seguro que quiere abandonar la pagina?</p>
+        <div>
+            <a href="#" id="aceptar">Aceptar</a>
+            <a href="#" id="cancelar">Cancelar</a>
+        </div>
+    </div>
+    `;
+    document.body.insertAdjacentHTML('beforeend', msj);
+
+    if(e.target.id == aceptar) {
+        console.log('aceptaste');
+    }
+
+    if(e.target.id == cancelar) {
+        console.log('cancelaste');
+    }
+
+});
