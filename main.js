6f92d36ff2b0366047fa4f54665e42fb047b456e
Josue Mendez <josuejoelmm@gmail.com>
2019-08-01 21:58:00 -0300

    actualizado el codigo

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-console.log('aaa');
+//AJAX = Async JS And XML
+// XHR/XHRU = XMLHttpRequest/Upload
+// Activex Object (deprecated)
+
+/**
+ * GET - POST - PUT - PATCH - DELETE - HEADERS
+ * - OPTIONS
+ * La Propiedad readyState de un objeto XHR determina
+ * en que estado esta el pedido
+ *  0 - Inicializado el pedido
+ *  1 - Configurado el pedido
+ *  2 - Headers enviados y recibidos
+ *  3 - 
+ *  4 -
+ * 
+ */
+
+let ajax = new XMLHttpRequest;
+
+ajax.addEventListener('readystatechange', () => {
+    console.log(`Nuevo Estado ${ajax.readyState}`);
+});
+
+// ajax.open(method -> string, url -> string);  
+ajax.open('GET', 'info.txt');
+
+//
+ajax.send();
+
