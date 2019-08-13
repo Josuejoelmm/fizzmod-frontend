f759fa3ede7b1154bc72dcfeba1fb44d7d4b10e2
Josue Mendez <josuejoelmm@gmail.com>
2019-08-13 17:03:58 -0300

    creada e instalada estructura de proyecto en react

---
+++ app/src/ui/layout/App.js
@@ -0,0 +1,68 @@
+import React , {Component,Fragment} from "react"
+import Header from "./Header"
+import Main from "./Main"
+import Footer from "./Footer"
+import Formulario from "../widgets/Formulario"
+// import Contador from "../widgets/Contador"
+
+class App extends Component {
+
+    constructor(){
+        super()
+        this.state = {
+            links : [ "perfil" , "portfolio" , "contacto" ],
+            mostrar : false,
+            usuario : "",
+            usuarios : []
+        }
+        this.aumentarContador = this.aumentarContador.bind(this)
+        this.mostrarFormulario = this.mostrarFormulario.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    aumentarContador(){
+        this.setState({ contador : this.state.contador + 1 })
+    }
+
+    mostrarFormulario(){
+        this.setState({ mostrar : !this.state.mostrar })
+    }
+
+    handleChange(e){
+        this.setState({ usuario : e.target.value })
+    }
+
+    handleSubmit(e){
+        e.preventDefault()
+        this.setState({ 
+            usuarios : [...this.state.usuarios,this.state.usuario] ,
+            usuario : ""
+        })
+    }
+
+    render(){
+        let {links,contador,mostrar,usuario,usuarios} = this.state
+        return (
+            <Fragment>
+                <Header links={links}/>
+                {/* <Contador/> */}
+                {mostrar ? 
+                    <Formulario 
+                        handleChange={this.handleChange}
+                        handleSubmit={this.handleSubmit}
+                        usuario={usuario}
+                        usuarios={usuarios}
+                    /> 
+                    : null
+                }
+                <button onClick={this.mostrarFormulario}>mostrar</button>
+                <Main/>
+                <Footer/>
+            </Fragment>
+        )
+    }
+}
+
+export default App 
+
